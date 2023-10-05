e830c1e053d19515ae7675bd535259af93d01954
Artem Sokolov <daironsib@gmail.com>
2023-10-05 14:14:19 +0200

    Update form for bills and add validation

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import React from 'react';
-import { useFormik, FormikProvider, FieldArray  } from 'formik';
-import { Container, TextField, Select, MenuItem, InputLabel, FormControl, Button, FormHelperText } from '@mui/material';
+import { useFormik, FormikProvider, FieldArray } from 'formik';
+import { Container, Box, TextField, Select, MenuItem, InputLabel, FormControl, Button, FormHelperText } from '@mui/material';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
@@ -10,58 +10,75 @@ import CancelIcon from '@mui/icons-material/Cancel';
 import * as Yup from 'yup';
 
 const CHARACTER_LIMIT = 31;
+const DATA_STRUCTURE = {
+  amount: '0.00',
+  account: '',
+  payee: '',
+  repeat: '',
+  note: '',
+  date: null
+};
 
 const validationSchema = Yup.object().shape({
   bills: Yup.array()
     .of(Yup.object().shape({
       amount: Yup
-        .number('Enter amount')
+        .number().typeError('Amount should be a number')
         .required('Amount is required'),
       account: Yup
-        .string('Please select your account')
-        .required('Account is required')
+        .number()
+        .required('Account is required'),
+      payee: Yup
+        .number()
+        .required('Payee is required'),
+      repeat: Yup
+        .number()
+        .required('Repeat is required'),
+      note: Yup
+        .string()
+        .required('Note is required'),
+      date: Yup
+        .date().typeError('Wrong date format')
+        .required('Date is required')   
     }))
 });
 
 const App = () => {
   const formik = useFormik({
     initialValues: {
-      bills: [{
-        amount: '0.00',
-        account: '',
-        payee: '',
-        repeat: '',
-        note: '',
-        date: null
-      }]
+      bills: [DATA_STRUCTURE]
     },
     validationSchema: validationSchema,
+    validateOnBlur: true,
+    validateOnChange: false,
     onSubmit: (values) => {
       alert(JSON.stringify(values, null, 2));
     }
   });
 
-  const addHandler = (arrayHelpers) => {
-    arrayHelpers.push({
-      amount: '0.00',
-      account: '',
-      payee: '',
-      repeat: '',
-      note: '',
-      date: null
-    })
+  const { values, errors, touched, handleSubmit, handleChange, handleBlur, setFieldValue, setFieldError } = formik;
+
+  const closeDatePickerHandler = (value, index) => {
+    if (!value) {
+      setFieldError(`bills[${index}].date`, 'Date is required!');
+    }
   }
+
+  const isFormEmpty = () => 
+    !values.bills[0].date ||
+    Object.keys(errors).length !== 0 ||
+    values.bills.length >= 5;
 
   return (
     <FormikProvider value={formik}>
-        <form onSubmit={formik.handleSubmit}>
-          <FieldArray name='bills' render={(arrayHelpers) => (
+        <form onSubmit={handleSubmit}>
+          <FieldArray name='bills' render={({remove, push}) => (
             <Container className='container'>
-              {formik.values.bills.map((bill, index) => (
+              {values.bills.map((bill, index) => (
                 <div className='bill' key={`bill-${index}`}>
                   {
                     index > 0 && 
-                    <CancelIcon className='bill__remove' onClick={() => arrayHelpers.remove(index)}/>
+                    <CancelIcon className='bill__remove' onClick={() => remove(index)}/>
                   }
                   <div className='bill__row bill__row--head'>
                     <TextField
@@ -69,26 +86,28 @@ const App = () => {
                       label='Amount'
                       variant='standard' 
                       name={`bills[${index}].amount`}
-                      value={formik.values.bills[index].amount}
-                      onChange={formik.handleChange}
+                      value={values.bills[index].amount}
+                      onChange={handleChange}
+                      onBlur={handleBlur}
                       InputProps={{ 
                         disableUnderline: true,
                         startAdornment: '$'
                       }}
-                      // error={formik.touched.bills[index].amount && Boolean(formik.errors.bills[index].amount)}
-                      // helperText={formik.touched.bills[index].amount && formik.errors.bills[index].amount}
+                      error={touched?.bills?.[index]?.amount && Boolean(errors?.bills?.[index]?.amount)}
+                      helperText={touched?.bills?.[index]?.amount && errors?.bills?.[index]?.amount}
                     />
                   </div>
                   <div className='bill__row'>
-                    <FormControl fullWidth>
+                    <FormControl error={touched?.bills?.[index]?.account && Boolean(errors?.bills?.[index]?.account)} fullWidth>
                       <InputLabel id='account-label'>From Account</InputLabel>
                       <Select
                         className='bill__account'
                         labelId='account-label'
                         label='From Account'
                         name={`bills[${index}].account`}
-                        value={formik.values.bills[index].account}
-                        onChange={formik.handleChange}
+                        value={values.bills[index].account}
+                        onChange={handleChange}
+                        onBlur={handleBlur}
                         fullWidth
                       >
                         <MenuItem value={1}>
@@ -100,60 +119,88 @@ const App = () => {
                           <span>$28,300.00</span>
                         </MenuItem>
                       </Select>
+                      {
+                        touched?.bills?.[index]?.account && Boolean(errors?.bills?.[index]?.account) && 
+                        <FormHelperText>{errors?.bills?.[index]?.account}</FormHelperText>
+                      }
                     </FormControl>
-                    <FormControl fullWidth>
+                    <FormControl error={touched?.bills?.[index]?.payee && Boolean(errors?.bills?.[index]?.payee)} fullWidth>
                       <InputLabel id='payee-label'>Payee</InputLabel>
                       <Select
                         labelId='payee-label'
                         label='Payee'
                         name={`bills[${index}].payee`}
-                        value={formik.values.bills[index].payee}
-                        onChange={formik.handleChange}
+                        value={values.bills[index].payee}
+                        onChange={handleChange}
+                        onBlur={handleBlur}
                         fullWidth
                       >
                         <MenuItem value={1}>London Hydro</MenuItem>
                         <MenuItem value={2}>Amsterdam High School</MenuItem>
                       </Select>
+                      {
+                        touched?.bills?.[index]?.payee && Boolean(errors?.bills?.[index]?.payee) && 
+                        <FormHelperText>{errors?.bills?.[index]?.payee}</FormHelperText>
+                      }
                       <FormHelperText>Last payment of $271.00 was on Dec 17, 2022.</FormHelperText>
                     </FormControl>
                   </div>
                   <div className='bill__row'>
-                  <LocalizationProvider dateAdapter={AdapterDayjs}>
-                    <DatePicker 
-                      className='bill__date'
-                      label='Date' 
-                      name={`bills[${index}].date`}
-                      value={formik.values.bills[index].date}
-                      onChange={e => formik.handleChange(`bills[${index}].date`, e)}
-                      fullWidth
-                    />
-                  </LocalizationProvider>
-                    <FormControl fullWidth>
+                    <LocalizationProvider dateAdapter={AdapterDayjs}>
+                      <Box className='bill__date'>
+                        <DatePicker 
+                          label='Date' 
+                          name={`bills[${index}].date`}
+                          value={values.bills[index].date}
+                          format='DD.MM.YYYY'
+                          className={errors?.bills?.[index]?.date? 'has-error' : null}
+                          onChange={(value) => setFieldValue(`bills[${index}].date`, value, true)}
+                          onClose={() => closeDatePickerHandler(values.bills[index].date, index)}
+                          fullWidth
+                        />
+                        {
+                          errors?.bills?.[index]?.date && 
+                          <FormHelperText error>{errors?.bills?.[index]?.date}</FormHelperText>
+                        }
+                      </Box>
+                    </LocalizationProvider>
+                    <FormControl error={touched?.bills?.[index]?.repeat && Boolean(errors?.bills?.[index]?.repeat)} fullWidth>
                       <InputLabel id='repeat'>Repeat</InputLabel>
                       <Select
                         labelId='repeat'
                         label='Repeat'
                         name={`bills[${index}].repeat`}
-                        value={formik.values.bills[index].repeat}
-                        onChange={formik.handleChange}
+                        value={values.bills[index].repeat}
+                        onChange={handleChange}
+                        onBlur={handleBlur}
                         fullWidth
                       >
                         <MenuItem value={2}>Every 2 Month, Untill Oct 12, 2023</MenuItem>
                         <MenuItem value={6}>Every 6 Month, Untill Dec 25, 2024</MenuItem>
                       </Select>
+                      {
+                        touched?.bills?.[index]?.repeat && Boolean(errors?.bills?.[index]?.repeat) && 
+                        <FormHelperText>{errors?.bills?.[index]?.repeat}</FormHelperText>
+                      }
                     </FormControl>
-                    <TextField
-                      className='bill__note'
-                      label='Note'
-                      inputProps={{
-                        maxLength: CHARACTER_LIMIT
-                      }}
-                      helperText={`${bill.note.length}/${CHARACTER_LIMIT}`}
-                      name={`bills[${index}].note`}
-                      value={formik.values.bills[index].note}
-                      onChange={formik.handleChange}
-                      fullWidth
-                    />
+                    <FormControl className='bill__note' fullWidth>
+                      <TextField
+                        label='Note'
+                        inputProps={{
+                          maxLength: CHARACTER_LIMIT
+                        }}
+                        name={`bills[${index}].note`}
+                        value={values.bills[index].note}
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        helperText={`${bill.note.length}/${CHARACTER_LIMIT}`}
+                        error={touched?.bills?.[index]?.note && Boolean(errors?.bills?.[index]?.note)}
+                      />
+                      {
+                        touched?.bills?.[index]?.note && Boolean(errors?.bills?.[index]?.note) && 
+                        <FormHelperText error>{errors?.bills?.[index]?.note}</FormHelperText>
+                      }
+                    </FormControl>
                   </div>
                 </div>
               ))}
@@ -162,9 +209,9 @@ const App = () => {
                 color='primary'
                 variant='outlined'
                 startIcon={<AddIcon />}
+                onClick={() => push(DATA_STRUCTURE)}
+                disabled={isFormEmpty()}
                 fullWidth
-                onClick={() => addHandler(arrayHelpers)}
-                disabled={formik.values.bills.length >= 5}
               >
                 Add Another Bill
               </Button>
